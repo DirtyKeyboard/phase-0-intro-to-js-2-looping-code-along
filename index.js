854aabd2ca75377b68f2d231d491d1c6ae752498
Andrew Hawileh <andrewh829@gmail.com>
2022-10-04 23:14:16 -0500

    commit tree 66597ea9e59d72305fc59f8d4e13032551ef787b

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(names, type)
+{
+    const ret = [];
+    for (let i = 0; i < names.length; i++)
+    {
+       ret.push(`Thank you, ${names[i]}, for the wonderful ${type} gift!`)
+    }
+    return ret;
+}
+
+function countDown(a)
+{
+    while (a > -1)
+    {
+        console.log(a)
+        a-=1;
+    }
+}
